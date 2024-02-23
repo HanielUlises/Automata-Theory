765cdb3938429b1b21f43efcc6a2db25939d1254
Haniel Ulises <hanielulises2003@gmail.com>
2024-02-22 19:21:07 -0600

    First simple automaton

---
+++ Basics/Automaton/automaton.cpp
@@ -0,0 +1,5 @@
+#include "automaton.h"
+
+Automaton::Automaton (std::string word, State qo) : word(word) : initial_state(qo)
+    {}
+
---
+++ Basics/Automaton/main.cpp
@@ -0,0 +1,10 @@
+#include "automaton.h"
+
+void main (){
+    std::string user_input;
+
+    std::cout << "Type in a word: ";
+    std::cin >> user_input; std::cout << std::endl;
+
+    return;
+}
