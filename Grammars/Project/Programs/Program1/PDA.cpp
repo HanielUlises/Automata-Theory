50eb4cdbed58083becc4b6b0fbd2a43cc593c854
HanielUlises <hanielulises2003@gmail.com>
2024-06-18 19:36:23 -0600

    Pushdown automaton

--- Grammars/Project/Programs/Program1/PDA.cpp
+++ Grammars/Project/Programs/Program1/PDA.cpp
@@ -70,3 +70,99 @@ std::string generateRandomString(size_t max_length) {
     size_t n = rand() % (max_length / 2) + 1;
     return std::string(n, '0') + std::string(n, '1');
 }
+
+void PDA::visualize() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return;
+    }
+
+    if (TTF_Init() == -1) {
+        std::cerr << "TTF_Init Error: " << TTF_GetError() << std::endl;
+        SDL_Quit();
+        return;
+    }
+
+    SDL_Window *win = SDL_CreateWindow("PDA Visualization", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+    if (win == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return;
+    }
+
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == nullptr) {
+        SDL_DestroyWindow(win);
+        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return;
+    }
+
+    TTF_Font* font = TTF_OpenFont("LinLibertine_aBS.ttf", 24);
+    if (font == nullptr) {
+        std::cerr << "TTF_OpenFont Error: " << TTF_GetError() << std::endl;
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return;
+    }
+
+    for (const auto& id : id_sequence) {
+        SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+        SDL_RenderClear(ren);
+
+        drawStack(ren, id.stack_before, 350, 400, font);
+        if (id.input != '\0') {
+            drawInputSymbol(ren, id.input, 100, 400, font);
+        }
+        SDL_RenderPresent(ren);
+
+        SDL_Delay(1500);
+
+        SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+        SDL_RenderClear(ren);
+        drawStack(ren, id.stack_after, 350, 400, font);
+        SDL_RenderPresent(ren);
+
+        SDL_Delay(1500);
+    }
+
+    TTF_CloseFont(font);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    TTF_Quit();
+    SDL_Quit();
+}
+
+void PDA::drawStack(SDL_Renderer* renderer, const std::stack<char>& stack, int x, int y, TTF_Font* font) {
+    std::stack<char> temp = stack;
+    int i = 0;
+    while (!temp.empty()) {
+        SDL_Rect rect = { x, y - i * 40, 50, 40 };
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderDrawRect(renderer, &rect);
+        renderText(renderer, std::string(1, temp.top()), x + 15, y - i * 40 + 5, font, {0, 0, 0, 255});
+        temp.pop();
+        i++;
+    }
+}
+
+void PDA::drawInputSymbol(SDL_Renderer* renderer, char input, int x, int y, TTF_Font* font) {
+    SDL_Rect rect = { x, y, 50, 50 };
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderDrawRect(renderer, &rect);
+
+    renderText(renderer, std::string(1, input), x + 15, y + 10, font, {255, 255, 255, 255});
+}
+
+void PDA::renderText(SDL_Renderer* renderer, const std::string& text, int x, int y, TTF_Font* font, SDL_Color color) {
+    SDL_Surface* surface = TTF_RenderText_Solid(font, text.c_str(), color);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Rect rect = { x, y, surface->w, surface->h };
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(texture);
+}
--- Grammars/Project/Programs/Program1/PDA.h
+++ Grammars/Project/Programs/Program1/PDA.h
@@ -6,6 +6,8 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 class PDA {
 public:
@@ -14,6 +16,7 @@ public:
     bool transition(char input);
     bool processString(const std::string& str);
     void logIdSequence(const std::string& filename);
+    void visualize();
 
 private:
     struct ID {
@@ -28,6 +31,9 @@ private:
     std::vector<ID> id_sequence;
 
     std::string stackToString(const std::stack<char>& stack);
+    void drawStack(SDL_Renderer* renderer, const std::stack<char>& stack, int x, int y, TTF_Font* font);
+    void drawInputSymbol(SDL_Renderer* renderer, char input, int x, int y, TTF_Font* font);
+    void renderText(SDL_Renderer* renderer, const std::string& text, int x, int y, TTF_Font* font, SDL_Color color);
 };
 
 std::string generateRandomString(size_t max_length = 100000);
